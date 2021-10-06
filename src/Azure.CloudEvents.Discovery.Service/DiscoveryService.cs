95f8f87374fc18e11df42c257490e7dad6150db8
clemensv <clemensv@microsoft.com>
2021-10-06 12:58:31 +0200

    Split functions

--- src/Azure.CloudEvents.Discovery.Service/DiscoveryService.cs
+++ src/Azure.CloudEvents.Discovery.Service/DiscoveryService.cs
@@ -21,205 +21,225 @@ namespace Azure.CloudEvents.Discovery
             this.cosmosClient = cosmosClient;
         }
 
-        [Function("services")]
+        [Function("services_get")]
         public async Task<HttpResponseData> Services(
-            [HttpTrigger(AuthorizationLevel.User|AuthorizationLevel.Function|AuthorizationLevel.System, "get", "post", "delete", "options", Route = "services")]
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "services")]
             HttpRequestData req,
             ILogger log)
         {
-            if (req.Method.Equals("get", StringComparison.InvariantCultureIgnoreCase))
+            List<Service> services = new List<Service>();
+            var container = this.cosmosClient.GetContainer("discovery", "services");
+            using (FeedIterator<Service> resultSet = container.GetItemQueryIterator<Service>())
             {
-                List<Service> services = new List<Service>();
-                var container = this.cosmosClient.GetContainer("discovery", "services");
-                using (FeedIterator<Service> resultSet = container.GetItemQueryIterator<Service>())
+                while (resultSet.HasMoreResults)
                 {
-                    while (resultSet.HasMoreResults)
+                    FeedResponse<Service> response = await resultSet.ReadNextAsync();
+                    services.AddRange(response);
+                    if (response.Diagnostics != null)
                     {
-                        FeedResponse<Service> response = await resultSet.ReadNextAsync();
-                        services.AddRange(response);
-                        if (response.Diagnostics != null)
-                        {
-                            Console.WriteLine($" Diagnostics {response.Diagnostics.ToString()}");
-                        }
+                        Console.WriteLine($" Diagnostics {response.Diagnostics.ToString()}");
                     }
                 }
-                var res = req.CreateResponse(HttpStatusCode.OK);
-                await res.WriteAsJsonAsync(services);
-                return res;
             }
-            else if (req.Method.Equals("post", StringComparison.InvariantCultureIgnoreCase))
-            {
-                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-                List<Service> requestServices = JsonConvert.DeserializeObject<List<Service>>(requestBody);
-                List<Service> responseServices = new List<Service>();
-                if (requestServices == null)
-                {
-                    return req.CreateResponse(HttpStatusCode.BadRequest);
-                }
-
-                var container = this.cosmosClient.GetContainer("discovery", "services");
-
-                foreach (var service in requestServices)
-                {
-
-                    try
-                    {
-                        var existingItem = await container.ReadItemAsync<Service>(service.Id, new PartitionKey(service.Id));
-                        if (existingItem.StatusCode == HttpStatusCode.OK)
-                        {
-                            if ( service.Epoch <= existingItem.Resource.Epoch)
-                            {
-                                // define code & response
-                                return req.CreateResponse(HttpStatusCode.Conflict);
-                            }
-                            var result = await container.UpsertItemAsync<Service>(service);
-                            responseServices.Add(result.Resource);
-                        }
-                        else
-                        {
-
-                            var result = await container.CreateItemAsync<Service>(service);
-                            responseServices.Add(result.Resource);
-                        }
-                    }
-                    catch (CosmosException)
-                    {
-                        return req.CreateResponse(HttpStatusCode.BadRequest);
-
-                    }
-                }
-                var res = req.CreateResponse(HttpStatusCode.OK);
-                await res.WriteAsJsonAsync(responseServices);
-                return res;
-            }
-            else if (req.Method.Equals("delete", StringComparison.InvariantCultureIgnoreCase))
-            {
-                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-                List<Serviceinstance> requestServices = JsonConvert.DeserializeObject<List<Serviceinstance>>(requestBody);
-                List<Service> responseServices = new List<Service>();
-                if (requestServices == null)
-                {
-                    return req.CreateResponse(HttpStatusCode.BadRequest);
-                }
-
-                var container = this.cosmosClient.GetContainer("discovery", "services");
-
-                foreach (var service in requestServices)
-                {
-
-                    try
-                    {
-                        var existingItem = await container.ReadItemAsync<Service>(service.Id, new PartitionKey(service.Id));
-                        if (existingItem.StatusCode == HttpStatusCode.OK)
-                        {
-                            var result = await container.DeleteItemAsync<Service>(service.Id, new PartitionKey(service.Id));
-                            responseServices.Add(existingItem);
-                        }
-
-                    }
-                    catch (CosmosException)
-                    {
-                        return req.CreateResponse(HttpStatusCode.BadRequest);
-
-                    }
-                }
-                var res = req.CreateResponse(HttpStatusCode.OK);
-                await res.WriteAsJsonAsync(responseServices);
-                return res;
-            }
-            return req.CreateResponse(HttpStatusCode.MethodNotAllowed);
+            var res = req.CreateResponse(HttpStatusCode.OK);
+            await res.WriteAsJsonAsync(services);
+            return res;
         }
 
-        [Function("service")]
+        [Function("services_post")]
+        public async Task<HttpResponseData> ServicesPost(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "services")]
+            HttpRequestData req,
+            ILogger log)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            List<Service> requestServices = JsonConvert.DeserializeObject<List<Service>>(requestBody);
+            List<Service> responseServices = new List<Service>();
+            if (requestServices == null)
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            var container = this.cosmosClient.GetContainer("discovery", "services");
+
+            foreach (var service in requestServices)
+            {
+
+                try
+                {
+                    var existingItem = await container.ReadItemAsync<Service>(service.Id, new PartitionKey(service.Id));
+                    if (existingItem.StatusCode == HttpStatusCode.OK)
+                    {
+                        if (service.Epoch <= existingItem.Resource.Epoch)
+                        {
+                            // define code & response
+                            return req.CreateResponse(HttpStatusCode.Conflict);
+                        }
+                        var result = await container.UpsertItemAsync<Service>(service);
+                        responseServices.Add(result.Resource);
+                    }
+                    else
+                    {
+
+                        var result = await container.CreateItemAsync<Service>(service);
+                        responseServices.Add(result.Resource);
+                    }
+                }
+                catch (CosmosException)
+                {
+                    return req.CreateResponse(HttpStatusCode.BadRequest);
+
+                }
+            }
+            var res = req.CreateResponse(HttpStatusCode.OK);
+            await res.WriteAsJsonAsync(responseServices);
+            return res;
+
+        }
+
+        [Function("services_delete")]
+        public async Task<HttpResponseData> ServicesDelete(
+            [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "services")]
+            HttpRequestData req,
+            ILogger log)
+        {
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            List<Serviceinstance> requestServices = JsonConvert.DeserializeObject<List<Serviceinstance>>(requestBody);
+            List<Service> responseServices = new List<Service>();
+            if (requestServices == null)
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            var container = this.cosmosClient.GetContainer("discovery", "services");
+
+            foreach (var service in requestServices)
+            {
+
+                try
+                {
+                    var existingItem = await container.ReadItemAsync<Service>(service.Id, new PartitionKey(service.Id));
+                    if (existingItem.StatusCode == HttpStatusCode.OK)
+                    {
+                        var result = await container.DeleteItemAsync<Service>(service.Id, new PartitionKey(service.Id));
+                        responseServices.Add(existingItem);
+                    }
+
+                }
+                catch (CosmosException)
+                {
+                    return req.CreateResponse(HttpStatusCode.BadRequest);
+
+                }
+            }
+            var res = req.CreateResponse(HttpStatusCode.OK);
+            await res.WriteAsJsonAsync(responseServices);
+            return res;
+
+        }
+
+        [Function("service_get")]
         public async Task<HttpResponseData> Service(
-            [HttpTrigger(AuthorizationLevel.User|AuthorizationLevel.Function|AuthorizationLevel.System, "get", "post", "put", "delete", "options", Route = "services/{id}")]
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "services/{id}")]
             HttpRequestData req,
             string id,
             ILogger log)
         {
             var container = this.cosmosClient.GetContainer("discovery", "services");
 
-            if (req.Method.Equals("get", StringComparison.InvariantCultureIgnoreCase))
+            try
             {
-                try
-                {
 
-                    var existingItem = await container.ReadItemAsync<Service>(id, new PartitionKey(id));
-                    var res = req.CreateResponse(HttpStatusCode.OK);
-                    await res.WriteAsJsonAsync(existingItem.Resource);
-                    return res;
-                }
-                catch(CosmosException ce)
-                {
-                    if(ce.StatusCode == HttpStatusCode.NotFound)
-                    {
-                        return req.CreateResponse(HttpStatusCode.NotFound);
-                    }
-                    return req.CreateResponse(HttpStatusCode.InternalServerError);
-                }
+                var existingItem = await container.ReadItemAsync<Service>(id, new PartitionKey(id));
+                var res = req.CreateResponse(HttpStatusCode.OK);
+                await res.WriteAsJsonAsync(existingItem.Resource);
+                return res;
             }
-            else if (req.Method.Equals("put", StringComparison.InvariantCultureIgnoreCase) ||
-                req.Method.Equals("post", StringComparison.InvariantCultureIgnoreCase))
+            catch (CosmosException ce)
             {
-                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-                Service service = JsonConvert.DeserializeObject<Service>(requestBody);
-                try
+                if (ce.StatusCode == HttpStatusCode.NotFound)
                 {
-                    var existingItem = await container.ReadItemAsync<Service>(service.Id, new PartitionKey(service.Id));
-                    if (service.Epoch <= existingItem.Resource.Epoch)
-                    {
-                        // define code & response
-                        return req.CreateResponse(HttpStatusCode.Conflict);
-                    }
-                    var result1 = await container.UpsertItemAsync<Service>(service);
-                    var res1 = req.CreateResponse(HttpStatusCode.OK);
-                    await res1.WriteAsJsonAsync(result1.Resource);
-                    return res1;
+                    return req.CreateResponse(HttpStatusCode.NotFound);
                 }
-                catch (CosmosException ce)
-                {
-                    if (ce.StatusCode != HttpStatusCode.NotFound)
-                    {
-                        throw;
-                    }
-                }
+                return req.CreateResponse(HttpStatusCode.InternalServerError);
+            }
 
-                try
+        }
+
+        [Function("service_put")]
+        public async Task<HttpResponseData> ServicePut(
+            [HttpTrigger(AuthorizationLevel.Function, "post", "put", Route = "services/{id}")]
+            HttpRequestData req,
+            string id,
+            ILogger log)
+        {
+            var container = this.cosmosClient.GetContainer("discovery", "services");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            Service service = JsonConvert.DeserializeObject<Service>(requestBody);
+            try
+            {
+                var existingItem = await container.ReadItemAsync<Service>(service.Id, new PartitionKey(service.Id));
+                if (service.Epoch <= existingItem.Resource.Epoch)
                 {
-                    var result = await container.CreateItemAsync<Service>(service, new PartitionKey(service.Id));
-                    var res = req.CreateResponse(HttpStatusCode.OK);
-                    await res.WriteAsJsonAsync(result.Resource);
-                    return res;
+                    // define code & response
+                    return req.CreateResponse(HttpStatusCode.Conflict);
                 }
-                catch(CosmosException ce)
+                var result1 = await container.UpsertItemAsync<Service>(service);
+                var res1 = req.CreateResponse(HttpStatusCode.OK);
+                await res1.WriteAsJsonAsync(result1.Resource);
+                return res1;
+            }
+            catch (CosmosException ce)
+            {
+                if (ce.StatusCode != HttpStatusCode.NotFound)
                 {
-                    return req.CreateResponse(HttpStatusCode.BadRequest);
+                    throw;
                 }
             }
-            else if (req.Method.Equals("delete", StringComparison.InvariantCultureIgnoreCase))
+
+            try
             {
-                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-                Serviceinstance service = JsonConvert.DeserializeObject<Serviceinstance>(requestBody);
-                try
-                {
-                    var existingItem = await container.ReadItemAsync<Serviceinstance>(service.Id, new PartitionKey(service.Id));
-                    var result = await container.DeleteItemAsync<Service>(service.Id, new PartitionKey(service.Id));
-                    var res = req.CreateResponse(HttpStatusCode.OK);
-                    await res.WriteAsJsonAsync(existingItem.Resource);
-                    return res;
-                }
-                catch (CosmosException ce)
-                {
-                    if (ce.StatusCode == HttpStatusCode.NotFound)
-                    {
-                        return req.CreateResponse(HttpStatusCode.NotFound);
-                    }
-                    return req.CreateResponse(HttpStatusCode.InternalServerError);
-                }
+                var result = await container.CreateItemAsync<Service>(service, new PartitionKey(service.Id));
+                var res = req.CreateResponse(HttpStatusCode.OK);
+                await res.WriteAsJsonAsync(result.Resource);
+                return res;
             }
-            else
+            catch (CosmosException ce)
             {
-                return req.CreateResponse(HttpStatusCode.MethodNotAllowed);
+                return req.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+        }
+
+        [Function("service_delete")]
+        public async Task<HttpResponseData> ServiceDelete(
+            [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "services/{id}")]
+            HttpRequestData req,
+            string id,
+            ILogger log)
+        {
+            var container = this.cosmosClient.GetContainer("discovery", "services");
+
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            Serviceinstance service = JsonConvert.DeserializeObject<Serviceinstance>(requestBody);
+            try
+            {
+                var existingItem = await container.ReadItemAsync<Serviceinstance>(service.Id, new PartitionKey(service.Id));
+                var result = await container.DeleteItemAsync<Service>(service.Id, new PartitionKey(service.Id));
+                var res = req.CreateResponse(HttpStatusCode.OK);
+                await res.WriteAsJsonAsync(existingItem.Resource);
+                return res;
+            }
+            catch (CosmosException ce)
+            {
+                if (ce.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return req.CreateResponse(HttpStatusCode.NotFound);
+                }
+                return req.CreateResponse(HttpStatusCode.InternalServerError);
             }
         }
     }
